94b731c93297f6dfb200f433f34c6497f1fa8125
starlaeve2 <starlaeve2@gmail.com>
2026-06-26 11:31:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ credentials_dict = {
     "client_id": "106061085208077860075",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url = \"https://www.googleapis.com/oauth2/v1/certs\"",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sheets-bot%40mimetic-planet-500614-f2.iam.gserviceaccount.com",
     "universe_domain": "googleapis.com"
 }
