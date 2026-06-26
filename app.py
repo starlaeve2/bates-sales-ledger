8d70b7c670095b701b5d3a30121c5168fe777ba4
starlaeve2 <starlaeve2@gmail.com>
2026-06-26 10:49:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import json
 try:
     api_key = st.secrets["GEMINI_API_KEY"]
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 except Exception as e:
     st.error("Missing Gemini API Key. Please add it to Streamlit Secrets.")
 
